f69f5ebdbc2f5c3948007d837b4ad97afa6f6c94
wstypr <wsetypeermadi@gmail.com>
2025-04-29 14:58:01 +0700

    add filter and sort

--- src/Result.tsx
+++ src/Result.tsx
@@ -33,11 +33,14 @@ function Result() {
   const [dataCapaian, setDataCapaian] = useState<IDataCapaian[] | null>(null);
 
   useEffect(() => {
-    setDataCapaian(data);
+    const sortedData = data?.sort(
+      (a: IDataCapaian, b: IDataCapaian) => b.respondent - a.respondent
+    );
+    setDataCapaian(sortedData);
   }, [data]);
 
   return (
-    <div className="flex flex-col justify-center items-center max-w-3xl mx-auto p-2 lg:p-10 text-center">
+    <div className="flex flex-col justify-center items-center max-w-3xl mx-auto px-2 py-10 lg:px-10 text-center">
       <h1 className="text-3xl font-bold">Capaian SPAK 2025</h1>
       <p className="text-lg mb-8">Pemerintah Kabupaten Mojokerto</p>
       {isLoading && <p className="text-center">Memuat data ...</p>}
@@ -47,39 +50,54 @@ function Result() {
         </p>
       )}
       {dataCapaian && (
-        <Table>
-          <TableHeader className="bg-slate-700">
-            <TableRow>
-              <TableHead className="text-left text-white px-5 py-2 whitespace-normal">
-                PERANGKAT DAERAH/UNIT KERJA
-              </TableHead>
-              <TableHead className="text-right text-white px-5 py-2 whitespace-normal">
-                Jumlah Responden
-              </TableHead>
-              <TableHead className="text-right text-white px-5 py-2 whitespace-normal">
-                Nilai IPAK
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {dataCapaian.map((capaian) => (
-              <TableRow
-                key={capaian.opd}
-                className="hover:bg-slate-500 hover:text-white"
-              >
-                <TableCell className="text-left whitespace-normal px-5 py-2">
-                  {capaian.opd}
-                </TableCell>
-                <TableCell className="text-right px-5 py-2">
-                  {capaian.respondent}
-                </TableCell>
-                <TableCell className="text-right font-medium px-5 py-2">
-                  {capaian.score.toFixed(2)}
-                </TableCell>
+        <>
+          <input
+            type="search"
+            className="px-4 py-2 border-1 border-gray-300 rounded-sm w-full mb-5"
+            placeholder="Cari Perangkat Daerah/Unit Kerja"
+            onKeyDown={(e) => {
+              const value = (e.target as HTMLInputElement).value.toLowerCase();
+              setDataCapaian(
+                data?.filter((row: IDataCapaian) =>
+                  row.opd.toLowerCase().includes(value)
+                )
+              );
+            }}
+          />
+          <Table>
+            <TableHeader className="bg-slate-700">
+              <TableRow>
+                <TableHead className="text-left text-white px-5 py-2 whitespace-normal">
+                  PERANGKAT DAERAH/UNIT KERJA
+                </TableHead>
+                <TableHead className="text-right text-white px-5 py-2 whitespace-normal">
+                  Jumlah Responden
+                </TableHead>
+                <TableHead className="text-right text-white px-5 py-2 whitespace-normal">
+                  Nilai IPAK
+                </TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {dataCapaian.map((capaian) => (
+                <TableRow
+                  key={capaian.opd}
+                  className="hover:bg-slate-500 hover:text-white"
+                >
+                  <TableCell className="text-left whitespace-normal px-5 py-2">
+                    {capaian.opd}
+                  </TableCell>
+                  <TableCell className="text-right px-5 py-2">
+                    {capaian.respondent}
+                  </TableCell>
+                  <TableCell className="text-right font-medium px-5 py-2">
+                    {capaian.score.toFixed(2)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </>
       )}
     </div>
   );
